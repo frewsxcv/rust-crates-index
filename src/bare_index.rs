80f89b7842e6203df3f8c73a002c7126368813fd
Jake Shadle <jake.shadle@embark-studios.com>
2020-09-12 11:15:39 +0200

    Add some tests/fixes

--- src/bare_index.rs
+++ src/bare_index.rs
@@ -7,6 +7,8 @@ pub struct BareIndex {
 }
 
 impl BareIndex {
+    /// Creates a bare index from a provided URL, opening the same location on
+    /// disk that cargo uses for that registry index.
     pub fn from_url(url: &str) -> Result<Self, Error> {
         let (dir_name, canonical_url) = url_to_local_dir(url)?;
         let mut path = home::cargo_home().unwrap_or_default();
@@ -18,6 +20,14 @@ impl BareIndex {
             path,
             url: canonical_url,
         })
+    }
+
+    /// Creates a bare index at the provided path with the specified repository URL.
+    pub fn with_path(path: PathBuf, url: &str) -> Self {
+        Self {
+            path,
+            url: url.to_owned(),
+        }
     }
 
     /// Creates and index for the default crates.io registry
@@ -63,7 +73,7 @@ impl<'a> BareIndexRepo<'a> {
         }
 
         let repo = git2::Repository::open(&index.path)?;
-        let head = repo.refname_to_id("FETCH_HEAD")?;
+        let head = repo.refname_to_id("FETCH_HEAD").or_else(|_| repo.refname_to_id("HEAD"))?;
         let head_str = head.to_string();
 
         let tree = {
@@ -82,6 +92,9 @@ impl<'a> BareIndexRepo<'a> {
         })
     }
 
+    /// Fetches latest from the remote index repository. Note that using this
+    /// method will mean no cache entries will be used, if a new commit is fetched
+    /// from the repository, as their commit version will no longer match.
     pub fn fetch(&mut self) -> Result<(), Error> {
         {
             let mut origin_remote = self.repo
@@ -91,7 +104,7 @@ impl<'a> BareIndexRepo<'a> {
             origin_remote.fetch(&["master"], Some(&mut crate::fetch_opts()), None)?;
         }
 
-        let head = self.repo.refname_to_id("FETCH_HEAD")?;
+        let head = self.repo.refname_to_id("FETCH_HEAD").or_else(|_| self.repo.refname_to_id("HEAD"))?;
         let head_str = head.to_string();
 
         let commit = self.repo.find_commit(head)?;
@@ -106,6 +119,9 @@ impl<'a> BareIndexRepo<'a> {
         Ok(())
     }
 
+    /// Reads a crate from the index, it will attempt to use a cached entry if
+    /// one is available, otherwise it will fallback to reading the crate
+    /// directly from the git blob containing the crate information.
     pub fn krate(&self, name: &str) -> Option<Crate> {
         let rel_path = match crate::crate_name_to_relative_path(name) {
             Some(rp) => rp,
@@ -281,5 +297,93 @@ mod test {
                 crate::INDEX_GIT_URL.to_owned()
             )
         );
+    }
+
+    #[test]
+    fn clones_bare_index() {
+        use super::BareIndex;
+
+        let tmp_dir = tempdir::TempDir::new("clones_bare_index").unwrap();
+
+        let index = BareIndex::with_path(tmp_dir.path().to_owned(), crate::INDEX_GIT_URL);
+
+        let mut repo = index.open_or_clone().expect("Failed to clone crates.io index");
+
+        fn test_sval(repo: &super::BareIndexRepo<'_>) {
+            let krate = repo
+                .krate("sval")
+                .expect("Could not find the crate sval in the index");
+
+            let version = krate
+                .versions()
+                .iter()
+                .find(|v| v.version() == "0.0.1")
+                .expect("Version 0.0.1 of sval does not exist?");
+            let dep_with_package_name = version
+                .dependencies()
+                .iter()
+                .find(|d| d.name() == "serde_lib")
+                .expect("sval does not have expected dependency?");
+            assert_ne!(
+                dep_with_package_name.name(),
+                dep_with_package_name.package().unwrap()
+            );
+            assert_eq!(
+                dep_with_package_name.crate_name(),
+                dep_with_package_name.package().unwrap()
+            );
+        }
+
+        test_sval(&repo);
+
+        repo.fetch().expect("Failed to fetch crates.io index");
+
+        test_sval(&repo);
+    }
+
+    #[test]
+    fn opens_bare_index() {
+        use super::BareIndex;
+
+        let tmp_dir = tempdir::TempDir::new("opens_bare_index").unwrap();
+
+        let index = BareIndex::with_path(tmp_dir.path().to_owned(), crate::INDEX_GIT_URL);
+
+        {
+            let _ = index.open_or_clone().expect("Failed to clone crates.io index");
+        }
+
+        let mut repo = index.open_or_clone().expect("Failed to open crates.io index");
+
+        fn test_sval(repo: &super::BareIndexRepo<'_>) {
+            let krate = repo
+                .krate("sval")
+                .expect("Could not find the crate sval in the index");
+
+            let version = krate
+                .versions()
+                .iter()
+                .find(|v| v.version() == "0.0.1")
+                .expect("Version 0.0.1 of sval does not exist?");
+            let dep_with_package_name = version
+                .dependencies()
+                .iter()
+                .find(|d| d.name() == "serde_lib")
+                .expect("sval does not have expected dependency?");
+            assert_ne!(
+                dep_with_package_name.name(),
+                dep_with_package_name.package().unwrap()
+            );
+            assert_eq!(
+                dep_with_package_name.crate_name(),
+                dep_with_package_name.package().unwrap()
+            );
+        }
+
+        test_sval(&repo);
+
+        repo.fetch().expect("Failed to fetch crates.io index");
+
+        test_sval(&repo);
     }
 }
