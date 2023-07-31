a7801b0576c5be3fe77befa71559c2518a0ab9c7
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-07-31 11:28:14 +0200

    avoid allocation of vector for separator indices
    We know it's not going to be too many.

--- src/names.rs
+++ src/names.rs
@@ -4,33 +4,37 @@ pub struct Names {
     count: u16,
     max_count: u16,
     chars: Vec<char>,
-    separator_indexes: Vec<usize>,
+    separator_indexes: [usize; 17],
+    separator_count: usize,
 }
 
 impl Names {
     /// Creates a new iterator over all permutations of `-` and `_` of the given `name`,
     /// or `None` if there are more than 15 `-` or `_` characters.
     pub fn new(name: &str) -> Option<Names> {
-        let mut separator_indexes = vec![];
+        let mut separator_indexes = [0; 17];
 
+        let mut separator_count = 0;
         let chars: Vec<char> = name
             .chars()
             .enumerate()
             .map(|(index, char)| {
                 if char == '-' || char == '_' {
-                    separator_indexes.push(index);
-                    return '-';
+                    separator_indexes[separator_count] = index;
+                    separator_count += 1;
+                    '-'
+                } else {
+                    char
                 }
-
-                char
             })
             .collect();
 
         Some(Names {
             count: 0,
-            max_count: 2u16.checked_pow(separator_indexes.len().try_into().ok()?)?,
+            max_count: 2u16.checked_pow(separator_count.try_into().ok()?)?,
             chars,
             separator_indexes,
+            separator_count,
         })
     }
 }
@@ -43,7 +47,7 @@ impl Iterator for Names {
             return None;
         }
 
-        for (index, string_index) in self.separator_indexes.iter().enumerate() {
+        for (index, string_index) in self.separator_indexes[..self.separator_count].iter().enumerate() {
             let char = if self.count & (1 << index) == 0 { '_' } else { '-' };
             *self.chars.get_mut(*string_index).unwrap() = char;
         }
