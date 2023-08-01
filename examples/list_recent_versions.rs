fdf663e9914752393bd456bf98cf6e9f6486dc4b
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-08-01 12:46:05 +0200

    refactor

--- examples/list_recent_versions.rs
+++ examples/list_recent_versions.rs
@@ -1,87 +1,86 @@
-//! prints the latest versions of a give crate name
+//! Print the 5 most recent versions of a give crate name.
 //!
-//! it first checks all possible [names](Names) locally <br>
-//! if it couldnt not find the crate locally it starts fetching the most likely [names](Names)
-//!
-//! how to run: <br>
-//! `cargo run --example get_versions_cli <crate-name>`
+//! It first checks all possible [names](Names) using the local cache and on failure
+//! updates the cache by fetching the most likely [names](Names) from the sparse registry.
 
 use crates_index::{Crate, Names, SparseIndex};
-use std::env;
+use std::error::Error;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn main() -> Result<(), Box<dyn Error>> {
+    let sparse_index = SparseIndex::new_cargo_default()?;
+    let mut count = 0;
+    for name in std::env::args().skip(1) {
+        count += 1;
+        let krate = match find_locally(&name, &sparse_index)? {
+            Some(krate) => krate,
+            None => fetch_crate(&name, &sparse_index)?.ok_or_else(|| format!("could not find '{name}'"))?,
+        };
 
-    let name = match args.get(1) {
-        None => {
-            println!("missing crate-name argument, please run the example like shown in the doc");
-            return;
-        }
-        Some(name) => name,
-    };
+        print_crate(krate);
+    }
 
-    let sparse_index = SparseIndex::new_cargo_default().unwrap();
-
-    match find_locally(name, &sparse_index) {
-        Some(krate) => print_crate(krate),
-        None => match fetch_crate(name, &sparse_index) {
-            None => println!("could not find '{}'", name),
-            Some(krate) => print_crate(krate),
-        },
+    if count == 0 {
+        Err("Please provide one or more crate names to lookup".into())
+    } else {
+        Ok(())
     }
 }
 
-/// loop though all possible Name permutations and return the crate if found
+/// Loop though all possible permutations of `name` and return the crate if found.
+/// This is feasible as local lookups are fast.
 ///
-/// more info about [Name permutations](Names)
-fn find_locally(name: &str, sparse_index: &SparseIndex) -> Option<Crate> {
-    for name in Names::new(name).unwrap() {
-        println!("checking for '{}' locally", name);
+/// Read more about [name permutations](Names).
+fn find_locally(name: &str, sparse_index: &SparseIndex) -> Result<Option<Crate>, Box<dyn Error>> {
+    for name in names(name)? {
+        eprintln!("checking for '{}' locally", name);
 
         if let Ok(krate) = sparse_index.crate_from_cache(&name) {
-            return Some(krate);
+            return Ok(Some(krate));
         }
     }
-
-    None
+    Ok(None)
 }
 
-/// fetch the first 3 Name permutations and return the crate if found
+/// Fetch the first 3 permutations of `name` from the sparse registry and return the crate if found.
 ///
-/// here we only use the first 3 names as these are the most likely to be correct
-/// and we skip the rest to minimize the performance hit if a crate does not exist
+/// Here we only use the first 3 names which are most likely to be correct
+/// and  skip the rest to minimize the performance hit if a crate does not exist.
 ///
-/// more info about [Name permutations](Names)
-fn fetch_crate(name: &str, sparse_index: &SparseIndex) -> Option<Crate> {
-    for name in Names::new(name).unwrap().take(3) {
-        println!("fetching for '{}'", name);
+/// Read more about [name permutations](Names).
+fn fetch_crate(name: &str, sparse_index: &SparseIndex) -> Result<Option<Crate>, Box<dyn Error>> {
+    for name in names(name)? {
+        eprintln!("fetching for '{}'", name);
 
-        if let Some(krate) = update(&name, sparse_index) {
-            return Some(krate);
+        if let Some(krate) = update(&name, sparse_index)? {
+            return Ok(Some(krate));
         }
     }
-
-    None
+    Ok(None)
 }
 
-/// a small helper to update sparse data
-fn update(name: &str, index: &SparseIndex) -> Option<Crate> {
-    let request: ureq::Request = index.make_cache_request(name).unwrap().into();
+fn names(name: &str) -> Result<impl Iterator<Item = String>, Box<dyn Error>> {
+    Ok(Names::new(name)
+        .ok_or_else(|| "Too many hyphens in crate name")?
+        .take(3))
+}
+
+/// Create a request to the sparse `index` and parse the response with the side-effect of yielding
+/// the desired crate and updating the local cache.
+fn update(name: &str, index: &SparseIndex) -> Result<Option<Crate>, Box<dyn Error>> {
+    let request: ureq::Request = index.make_cache_request(name)?.into();
 
     let response: http::Response<String> = match request.call() {
         Ok(response) => response.into(),
-        Err(_) => return None,
+        Err(_) => return Ok(None),
     };
 
     let (parts, body) = response.into_parts();
     let response = http::Response::from_parts(parts, body.into_bytes());
 
-    index.parse_cache_response(name, response, true).unwrap()
+    Ok(index.parse_cache_response(name, response, true)?)
 }
 
-/// a small helper to print the found crate in a formatted way
 fn print_crate(krate: Crate) {
-    println!();
     println!("{}", krate.name());
 
     let versions = krate
@@ -96,5 +95,7 @@ fn print_crate(krate: Crate) {
 
     if krate.versions().len() > 5 {
         println!(", ...")
+    } else {
+        println!()
     }
 }
